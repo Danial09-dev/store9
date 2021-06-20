fbad0712d77a4b413790a50ceef79fadb7af42f1
Danial09-dev <danialzafar009@gmail.com>
2021-06-20 18:58:06 +0500

    First Commit

--- git-new-app.bat
+++ git-new-app.bat
@@ -2,6 +2,6 @@ cls
 git init
 git add .
 git commit -m "First Commit"
-git remote add origin https://github.com/<user>/<repo-name>.git
+git remote add origin https://github.com/Danial09-dev/store9.git
 git push origin main
 git status
